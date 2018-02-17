44760b5617dd7c0bef696b122f3ab67451b0fc26
lufte <javierayres@gmail.com>
2018-02-17 20:14:30 -0300

    Implement "support widgets" logic

--- test.py
+++ test.py
@@ -89,5 +89,119 @@ class IndependentBlocksTestCase3(unittest.TestCase):
                          [0, 1, 2, 3])
 
 
+class SupportWidgetsTestCase(unittest.TestCase):
+
+    #  ┌───┬───┬───────┬───┐
+    #  │   │ 1 │   2   │ 3 │
+    #  │ 0 ├───┼───┬───┼───┤
+    #  │   │ 4 │   │ 6 │ 7 │
+    #  ├───┴───┤ 5 ├───┼───┤
+    #  │   8   │   │ 9 │   │
+    #  ├───┬───┼───┴───┤10 │
+    #  │11 │12 │  13   │   │
+    #  ├───┴───┴───────┼───┤
+    #  │       14      │15 │
+    #  └───────────────┴───┘
+    def setUp(self):
+        self.app = QApplication([])
+        self.layout = QTilingLayout()
+        self.widgets = [QWidget() for _ in range(16)]
+        self.layout.addWidget(self.widgets[0], 0, 0, 2, 1)
+        self.layout.addWidget(self.widgets[1], 0, 1, 1, 1)
+        self.layout.addWidget(self.widgets[2], 0, 2, 1, 2)
+        self.layout.addWidget(self.widgets[3], 0, 4, 1, 1)
+        self.layout.addWidget(self.widgets[4], 1, 1, 1, 1)
+        self.layout.addWidget(self.widgets[5], 1, 2, 2, 1)
+        self.layout.addWidget(self.widgets[6], 1, 3, 1, 1)
+        self.layout.addWidget(self.widgets[7], 1, 4, 1, 1)
+        self.layout.addWidget(self.widgets[8], 2, 0, 1, 2)
+        self.layout.addWidget(self.widgets[9], 2, 3, 1, 1)
+        self.layout.addWidget(self.widgets[10], 2, 4, 2, 1)
+        self.layout.addWidget(self.widgets[11], 3, 0, 1, 1)
+        self.layout.addWidget(self.widgets[12], 3, 1, 1, 1)
+        self.layout.addWidget(self.widgets[13], 3, 2, 1, 2)
+        self.layout.addWidget(self.widgets[14], 4, 0, 1, 4)
+        self.layout.addWidget(self.widgets[15], 4, 4, 1, 1)
+
+    def test_horizontal_before_support_widgets(self):
+        supporters = self.layout._get_support_widgets(self.widgets[10], True,
+                                                      True)
+        self.assertIn(self.widgets[0], supporters)
+        self.assertNotIn(self.widgets[1], supporters)
+        self.assertNotIn(self.widgets[2], supporters)
+        self.assertNotIn(self.widgets[3], supporters)
+        self.assertIn(self.widgets[4], supporters)
+        self.assertIn(self.widgets[5], supporters)
+        self.assertNotIn(self.widgets[6], supporters)
+        self.assertNotIn(self.widgets[7], supporters)
+        self.assertIn(self.widgets[8], supporters)
+        self.assertIn(self.widgets[9], supporters)
+        self.assertNotIn(self.widgets[10], supporters)
+        self.assertIn(self.widgets[11], supporters)
+        self.assertIn(self.widgets[12], supporters)
+        self.assertIn(self.widgets[13], supporters)
+        self.assertNotIn(self.widgets[14], supporters)
+        self.assertNotIn(self.widgets[15], supporters)
+
+    def test_horizontal_after_support_widgets(self):
+        supporters = self.layout._get_support_widgets(self.widgets[8], True,
+                                                      False)
+        self.assertNotIn(self.widgets[0], supporters)
+        self.assertNotIn(self.widgets[1], supporters)
+        self.assertNotIn(self.widgets[2], supporters)
+        self.assertNotIn(self.widgets[3], supporters)
+        self.assertNotIn(self.widgets[4], supporters)
+        self.assertIn(self.widgets[5], supporters)
+        self.assertIn(self.widgets[6], supporters)
+        self.assertIn(self.widgets[7], supporters)
+        self.assertNotIn(self.widgets[8], supporters)
+        self.assertIn(self.widgets[9], supporters)
+        self.assertIn(self.widgets[10], supporters)
+        self.assertNotIn(self.widgets[11], supporters)
+        self.assertNotIn(self.widgets[12], supporters)
+        self.assertNotIn(self.widgets[13], supporters)
+        self.assertNotIn(self.widgets[14], supporters)
+        self.assertNotIn(self.widgets[15], supporters)
+
+    def test_vertical_before_support_widgets(self):
+        supporters = self.layout._get_support_widgets(self.widgets[12], False,
+                                                      True)
+        self.assertIn(self.widgets[0], supporters)
+        self.assertIn(self.widgets[1], supporters)
+        self.assertNotIn(self.widgets[2], supporters)
+        self.assertNotIn(self.widgets[3], supporters)
+        self.assertIn(self.widgets[4], supporters)
+        self.assertNotIn(self.widgets[5], supporters)
+        self.assertNotIn(self.widgets[6], supporters)
+        self.assertNotIn(self.widgets[7], supporters)
+        self.assertIn(self.widgets[8], supporters)
+        self.assertNotIn(self.widgets[9], supporters)
+        self.assertNotIn(self.widgets[10], supporters)
+        self.assertNotIn(self.widgets[11], supporters)
+        self.assertNotIn(self.widgets[12], supporters)
+        self.assertNotIn(self.widgets[13], supporters)
+        self.assertNotIn(self.widgets[14], supporters)
+        self.assertNotIn(self.widgets[15], supporters)
+
+    def test_vertical_after_support_widgets(self):
+        supporters = self.layout._get_support_widgets(self.widgets[2], False,
+                                                      False)
+        self.assertNotIn(self.widgets[0], supporters)
+        self.assertNotIn(self.widgets[1], supporters)
+        self.assertNotIn(self.widgets[2], supporters)
+        self.assertNotIn(self.widgets[3], supporters)
+        self.assertNotIn(self.widgets[4], supporters)
+        self.assertIn(self.widgets[5], supporters)
+        self.assertIn(self.widgets[6], supporters)
+        self.assertNotIn(self.widgets[7], supporters)
+        self.assertNotIn(self.widgets[8], supporters)
+        self.assertIn(self.widgets[9], supporters)
+        self.assertNotIn(self.widgets[10], supporters)
+        self.assertNotIn(self.widgets[11], supporters)
+        self.assertNotIn(self.widgets[12], supporters)
+        self.assertIn(self.widgets[13], supporters)
+        self.assertIn(self.widgets[14], supporters)
+        self.assertNotIn(self.widgets[15], supporters)
+
 if __name__ == '__main__':
     unittest.main()
--- tilinglayout.py
+++ tilinglayout.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QGridLayout
+from bisect import bisect
 import itertools
 
 class QTilingLayout(QGridLayout):
@@ -12,6 +13,9 @@ class QTilingLayout(QGridLayout):
 
     def vsplit(self, old_widget, new_widget, put_before=False):
         self._split(old_widget, new_widget, False, put_before=put_before)
+
+    def _get_critical_block(self, horizontal, i, j):
+        pass
 
     def _get_independent_blocks(self, horizontal):
         """
@@ -49,10 +53,70 @@ class QTilingLayout(QGridLayout):
 
         return blocks
 
+    def _get_min_span(start, end, horizontal):
+        min_span = None
+        for outer in range(start, end):
+            for inner in range(0, self.rowCount() if horizontal else
+                    self.columnCount()):
+                pos = (inner, outer) if horizontal else (outer, inner)
+                item = self.itemAtPosition(*pos)
+                item_pos = self.getItemPosition(self.indexOf(item.widget()))
+                span = item_pos[2] if horizontal else item_pos[3]
+                if not min_span or span < min_span:
+                    min_span = span
+        return min_span
+
+    def _get_support_widgets(self, widget, horizontal, before):
+        supporters = set()
+        pos = self.getItemPosition(self.indexOf(widget))
+        if horizontal:
+            pivot = pos[1] - 1 if before else pos[1] + pos[3]
+            start = pos[0]
+            end = pos[0] + pos[2]
+        else:
+            pivot = pos[0] - 1 if before else pos[0] + pos[2]
+            start = pos[1]
+            end = pos[1] + pos[3]
+        upper_limit = self.columnCount() if horizontal else self.rowCount()
+        within_limits = (pivot >= 0) if before else (pivot < upper_limit)
+        if within_limits:
+            for index in range(start, end):
+                curr_pos = (index, pivot) if horizontal else (pivot, index)
+                item = self.itemAtPosition(*curr_pos)
+                supporters.add(item.widget())
+        return supporters.union(
+            *(self._get_support_widgets(w, horizontal, before)
+              for w in supporters)
+        )
+
     def _split(self, old_widget, new_widget, horizontal, put_before=False):
-        blocks = get_independent_blocks(horizontal)
+        ind_blocks = self._get_independent_blocks(horizontal)
+        support_widgets = self._get_support_widgets(old_widget)
         curr_pos = self.getItemPosition(self.indexOf(old_widget))
         span = curr_pos[2] if horiztonal else curr_pos[3]
+        curr_block_index = bisect(blocks,
+                                  curr_pos[1] if horizontal else curr_pos[0])
+        curr_block_pos = (blocks[curr_block_index - 1],
+                          blocks[curr_block_index])
+        min_span = _get_min_span(*curr_block_pos, horizontal)
+
+        # Dentro del bloque independiente afectado, busco la columna (o fila)
+        # de soporte, es decir, todos los widgets sobre los cuales el widget
+        # actual está apoyado, y todos los que están apoyados sobre él. De esos
+        # widgets, solo aquellos que no tengan el tamaño mínimo podrán
+        # achicarse, y si el widget actual ya tiene el tamaño mínimo se busca
+        # un nuevo tamaño mínimo.
+        # Si el widget actual tiene el doble o más espacio que el span mínimo,
+        # entonces solo él se achica
+
+
+
+
+
+
+
+
+
         if span == self._unity:
             # we need to insert a new row/column
             pass
