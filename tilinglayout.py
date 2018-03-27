f5c9e5cd70929aab0712d6285778ae3f87e1d60e
lufte <javierayres@gmail.com>
2018-03-27 15:25:36 -0300

    Make max span a constructor argument

--- tilinglayout.py
+++ tilinglayout.py
@@ -16,13 +16,16 @@ def lcm(*numbers):
     return reduce(lambda a, b: (a * b) // gcd_(a, b), numbers, 1)
 
 
+class SplitLimitException(Exception):
+    pass
+
+
 class QTilingLayout(QGridLayout):
 
-    MAX_SPAN = 210
-
-    def __init__(self, widget, *args, **kwargs):
+    def __init__(self, widget, *args, max_span=60, **kwargs):
         super().__init__(*args, **kwargs)
-        self._add_widget(widget, 0, 0, self.MAX_SPAN, self.MAX_SPAN, False)
+        self.max_span = max_span
+        self._add_widget(widget, 0, 0, self.max_span, self.max_span, False)
 
     def _add_widget(self, widget, row, col, rowspan, colspan, transpose):
         """Invokes QGridLayout.addWidget on a possibly transposed grid.
@@ -90,7 +93,9 @@ class QTilingLayout(QGridLayout):
             return self.columnCount()
 
     def hsplit(self, old_widget, new_widget, put_before=False):
-        self._split(old_widget, new_widget, False, put_before=put_before)
+        self._split(old_widget, new_widget, put_before, False)
 
     def vsplit(self, old_widget, new_widget, put_before=False):
-        self._split(old_widget, new_widget, True, put_before=put_before)
+        self._split(old_widget, new_widget, put_before, True)
+
+    def _split(self, old_widget, new_widget, put_before, transpose):
